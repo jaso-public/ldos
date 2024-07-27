9e934f318101468971132775a0507a30c83f11f6
jaso <jaso@cs.wisc.edu>
2024-07-27 16:28:12 -0500

    stuff before the cloudlab machine goes away

--- actions/simple-unmap.c
+++ actions/simple-unmap.c
@@ -10,7 +10,7 @@
 
 int main() {
     unsigned long addr = 0;  // Desired address or 0 for the kernel to choose
-    unsigned long len = 4096;  // Length of the mapping
+    unsigned long len = 4096 * 1024;  // Length of the mapping
     unsigned long prot = PROT_READ | PROT_WRITE;  // Protection flags
     unsigned long flags = MAP_PRIVATE | MAP_ANONYMOUS;  // Mapping flags
     unsigned long fd = -1;  // File descriptor, -1 for MAP_ANONYMOUS
@@ -18,6 +18,7 @@ int main() {
 
     // Call the custom system call
     unsigned long result = syscall(SYS_my_mmap, addr, len, prot, flags, fd, pgoff);
+    printf("my_mmap returned: %lu\n", result);
 
     // Check the result
     if (result == (unsigned long)-1) {
@@ -25,9 +26,13 @@ int main() {
         perror("my_mmap syscall failed");
         return 1;
     }
+
+    // make a hole in the allocated memory
+    addr = result + 8192;
     
     // Call the custom system call
-    result = syscall(SYS_my_munmap, addr, len);
+    result = syscall(SYS_my_munmap, addr, 8192);
+    printf("my_munmap returned: %lu\n", result);
 
     // Check the result
     if (result) {
